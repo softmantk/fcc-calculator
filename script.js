3eb37b079a2f4932a6bcbe5720ac2a1f6bde5361
nikhilcm <rose12nikhl@gmail.com>
2017-03-28 20:41:53 +0530

    UI html css completed beta

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by SOFTMAN on 28-03-2017.
+ */
+var app = angular.module("calculator",[]);
+
+app.controller("calCtrl", function ($scope) {
+    
+    $scope.current = 5;
+    
+    
+    
+});
